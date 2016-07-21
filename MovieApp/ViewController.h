fe280cc90ef4b559be59429874d302745ca0d9d1
Samuel Maina Karumba <karumbas@ke.ibm.com>
2016-07-21 15:00:51 +0300

    Initial Commit

---
+++ MovieApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MovieApp
+//
+//  Created by Karumba Samuel on 21/07/2016.
+//  Copyright © 2016 IBM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MovieApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MovieApp
+//
+//  Created by Karumba Samuel on 21/07/2016.
+//  Copyright © 2016 IBM. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
